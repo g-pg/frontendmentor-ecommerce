8313bbeb07b0190ebf5bc9c1e89cab32f3d30d3f
g-pg <gabrielpgusso@gmail.com>
2023-03-06 00:13:36 -0300

    updated router for gh-pages

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<BrowserRouter basename="frontendmentor-ecommerce">
+		<BrowserRouter basename="/frontendmentor-ecommerce">
 			<App />
 		</BrowserRouter>
 	</React.StrictMode>
